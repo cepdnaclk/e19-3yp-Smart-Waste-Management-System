17844afcae67d15517e1e9dbd263100b8bb869fd
MOHAMED FAHMAN <e19105@eng.pdn.ac.lk>
2023-11-25 20:14:55 +0530

    Authentication

--- web-app/frontend/src/pages/Register.jsx
+++ web-app/frontend/src/pages/Register.jsx
@@ -1,64 +1,12 @@
-import { useState } from "react";
-import { useNavigate } from "react-router";
+import React from "react";
+import RegisterForm from "../components/Register/RegisterForm";
 
-function App() {
-  const navigate = useNavigate();
-
-  const [name, setName] = useState("");
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  async function registerUser(event) {
-    event.preventDefault();
-
-    const response = await fetch("http://localhost:1337/api/register", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        name,
-        username,
-        password,
-      }),
-    });
-
-    const data = await response.json();
-
-    if (data.status === "ok") {
-      navigate("/login");
-    }
-  }
-
+function Register() {
   return (
     <div>
-      <h1>Register</h1>
-      <form onSubmit={registerUser}>
-        <input
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-          type="text"
-          placeholder="Name"
-        />
-        <br />
-        <input
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          type="username"
-          placeholder="username"
-        />
-        <br />
-        <input
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          type="password"
-          placeholder="Password"
-        />
-        <br />
-        <input type="submit" value="Register" />
-      </form>
+      <RegisterForm />
     </div>
   );
 }
 
-export default App;
+export default Register;
