798397e57a48a18f7f58f2db4cd5649bb7e0d3ac
Hamshi Mohan <e19244@eng.pdn.ac.lk>
2024-01-06 21:15:19 +0530

    Login screen reverted to original

--- mobile-app/frontend/screens/LoginScreen.js
+++ mobile-app/frontend/screens/LoginScreen.js
@@ -10,182 +10,205 @@ import {
   Pressable,
   Alert,
 } from "react-native";
-import { MaterialIcons } from "@expo/vector-icons";  // Make sure this import is correct
+import { MaterialIcons } from "@expo/vector-icons";
 import { AntDesign } from "@expo/vector-icons";
-import AsyncStorage from "@react-native-async-storage/async-storage";
+import { useNavigation } from "@react-navigation/native";
 import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
-const LocalLoginScreen = ({ navigation }) => {
+const LoginScreen = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
-  const [loading, setLoading] = useState(false);
+  const navigation = useNavigation();
+  const handleLogin = () => {
+    const user = {
+      email: email,
+      password: password,
+    };
 
-  const handleLogin = async () => {
-    try {
-      setLoading(true);
-
-      const user = {
-        identifier: email,
-        password: password,
-      };
-
-      console.log("User object:", user);
-
-      const response = await axios.post("http://192.168.56.1:1337/api/login", user);
-
-      const token = response.data.jwt;
-      await AsyncStorage.setItem("authToken", token);
-      const userDetails = response.data.user;
-
-      if (userDetails && userDetails.confirmed && userDetails.blocked === false) {
-        console.log("Login successful. Navigating to your desired screen");
-        // Navigate to your desired screen
-      } else {
-        Alert.alert("Login Error", "Invalid Email or Password");
-      }
-    } catch (error) {
-      setLoading(false);
-      console.log("Login error:", error);
-
-      if (error.response) {
-        // The request was made and the server responded with a status code
-        // that falls out of the range of 2xx
-        Alert.alert("Login Error", error.response.data.message || "An error occurred.");
-      } else if (error.request) {
-        // The request was made but no response was received
-        Alert.alert("Login Error", "No response from the server. Please try again.");
-      } else {
-        // Something happened in setting up the request that triggered an Error
-        Alert.alert("Login Error", "An unexpected error occurred. Please try again later.");
-      }
-    } finally {
-      setLoading(false);
-    }
+    axios
+      .post("http://localhost:8000/login", user)
+      .then((response) => {
+        console.log(response);
+        const token = response.data.token;
+        AsyncStorage.setItem("authToken", token);
+        const role = response.data.role;
+        if (role === "Garbage Collector") {
+          navigation.replace("GarbageCollectorHomeScreen");
+        } else if (role === "House Owner") {
+          navigation.replace("HouseOwnerHomeScreen");
+        }
+      })
+      .catch((error) => {
+        Alert.alert("Login Error", "Invalid Email");
+        console.log(error);
+      });
   };
 
   return (
-    <SafeAreaView style={styles.container}>
-      <KeyboardAvoidingView behavior="padding" style={styles.keyboardAvoidingContainer}>
+    <SafeAreaView
+      style={{
+        flex: 1,
+        backgroundColor: "white",
+        alignItems: "center",
+        marginTop: 0,
+        backgroundSize: "cover",
+      }}
+    >
+      <View>
         <Image
           style={{ marginTop: 70, width: 250, height: 150 }}
           source={require("../assets/LoginScreen/head.png")}
         />
+      </View>
 
-        <Text style={styles.heading}>Login In To Your Account</Text>
+      <KeyboardAvoidingView>
+        <View style={{ alignItems: "center" }}>
+          <Text
+            style={{
+              marginTop: 20,
+              fontSize: 20,
+              fontWeight: "bold",
+              color: "green",
+            }}
+          >
+            Login In To Your Account
+          </Text>
+        </View>
 
-        <View style={styles.inputContainer}>
-          <View style={styles.inputWrapper}>
-            <MaterialIcons name="email" size={30} color="green" />
+        <View style={{ marginTop: 50 }}>
+          <View
+            style={{
+              alignItems: "center",
+              flexDirection: "row",
+              gap: 10,
+              backgroundColor: "#D0D0D0",
+              paddingVertical: 10,
+              borderRadius: 8,
+            }}
+          >
+            <MaterialIcons
+              style={{ marginLeft: 8 }}
+              name="email"
+              size={30}
+              color="green"
+            />
             <TextInput
               value={email}
               onChangeText={(text) => setEmail(text)}
-              style={styles.input}
+              style={{
+                color: "green",
+                marginVertical: 10,
+                width: 300,
+                fontSize: email ? 16 : 16,
+              }}
               placeholder="enter your Email"
             />
           </View>
         </View>
 
-        <View style={styles.inputContainer}>
-          <View style={styles.inputWrapper}>
-            <AntDesign name="lock" size={30} color="green" />
+        <View style={{ marginTop: 20 }}>
+          <View
+            style={{
+              alignItems: "center",
+              flexDirection: "row",
+              gap: 10,
+              backgroundColor: "#D0D0D0",
+              paddingVertical: 10,
+              borderRadius: 8,
+            }}
+          >
+            <AntDesign
+              style={{ marginLeft: 8 }}
+              name="lock"
+              size={30}
+              color="green"
+            />
             <TextInput
               value={password}
               onChangeText={(text) => setPassword(text)}
               secureTextEntry={true}
-              style={styles.input}
+              style={{
+                color: "green",
+                marginVertical: 10,
+                width: 300,
+                fontSize: password ? 16 : 16,
+              }}
               placeholder="enter your Password"
             />
           </View>
         </View>
 
-        <View style={styles.additionalInfoContainer}>
-          <Text style={styles.additionalInfoText}>Keep me logged in</Text>
-          <Text style={styles.additionalInfoText}>Forgot Password</Text>
+        <View
+          style={{
+            alignItems: "center",
+            marginTop: 15,
+            flexDirection: "row",
+            justifyContent: "space-between",
+          }}
+        >
+          <View>
+            <Text style={{ color: "gray" }}>Keep me logged in</Text>
+          </View>
+          <View>
+            <Text style={{ color: "green", fontWeight: "500" }}>
+              Forgot Password
+            </Text>
+          </View>
         </View>
 
-        <Pressable onPress={handleLogin} style={styles.loginButton}>
-          <Text style={styles.loginButtonText}>Login</Text>
+        <View style={{ marginTop: 100 }} />
+        <Pressable
+          //onPress={handleLogin}
+          onPress={() => navigation.navigate("common")}
+          style={{
+            width: 200,
+            backgroundColor: "green",
+            borderRadius: 10,
+            marginLeft: "auto",
+            marginRight: "auto",
+            padding: 20,
+          }}
+        >
+          <Text
+            style={{
+              textAlign: "center",
+              color: "white",
+              fontSize: 20,
+              fontWeight: "bold",
+            }}
+          >
+            Login
+          </Text>
         </Pressable>
 
-        <Pressable onPress={() => navigation.navigate("RegisterScreen")} style={styles.signupLink}>
-          <Text style={styles.signupLinkText}>Don't have an account? Sign Up</Text>
+        <Pressable
+          onPress={() => navigation.navigate("RegisterScreen")}
+          style={{ marginTop: 15 }}
+        >
+          <Text
+            style={{
+              textAlign: "center",
+              color: "gray",
+              fontSize: 16,
+            }}
+          >
+            <Text
+              style={{
+                textAlign: "center",
+                color: "green",
+                fontSize: 16,
+              }}
+            >
+              Don't have an account? Sign Up
+            </Text>
+          </Text>
         </Pressable>
       </KeyboardAvoidingView>
     </SafeAreaView>
   );
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: "white",
-    alignItems: "center",
-    justifyContent: "center",
-  },
-  keyboardAvoidingContainer: {
-    flex: 1,
-    width: "80%",
-    marginLeft: "auto",
-    marginRight: "auto",
-  },
-  heading: {
-    marginTop: 20,
-    fontSize: 20,
-    fontWeight: "bold",
-    color: "green",
-    marginBottom: 20,
-    textAlign: "center",
-  },
-  inputContainer: {
-    marginTop: 20,
-  },
-  inputWrapper: {
-    alignItems: "center",
-    flexDirection: "row",
-    gap: 10,
-    backgroundColor: "#D0D0D0",
-    paddingVertical: 10,
-    borderRadius: 8,
-  },
-  input: {
-    color: "green",
-    marginVertical: 10,
-    width: 300,
-    fontSize: 16,
-  },
-  additionalInfoContainer: {
-    marginTop: 15,
-    flexDirection: "row",
-    justifyContent: "space-between",
-  },
-  additionalInfoText: {
-    color: "gray",
-  },
-  loginButton: {
-    width: 200,
-    backgroundColor: "green",
-    borderRadius: 10,
-    marginLeft: "auto",
-    marginRight: "auto",
-    padding: 20,
-    marginTop: 100,
-  },
-  loginButtonText: {
-    textAlign: "center",
-    color: "white",
-    fontSize: 20,
-    fontWeight: "bold",
-  },
-  signupLink: {
-    marginTop: 15,
-  },
-  signupLinkText: {
-    textAlign: "center",
-    color: "gray",
-    fontSize: 16,
-    color: "green",
-  },
-});
+export default LoginScreen;
 
-export default LocalLoginScreen;
-
+const styles = StyleSheet.create({});
