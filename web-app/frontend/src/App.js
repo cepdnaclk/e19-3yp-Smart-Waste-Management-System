2a93d7b369207423f8f2d33772f6ebb6ff3cdaa3
Mohmaed Fahman <115540264+MohamedFahman@users.noreply.github.com>
2024-01-06 19:02:56 +0530

    Authendication for Admin updated

--- web-app/frontend/src/App.js
+++ web-app/frontend/src/App.js
@@ -17,31 +17,13 @@ const App = () => {
     <div>
       <BrowserRouter>
         <Routes>
-          <Route path="/bins" element={user ? <Bin /> : <Navigate to="/" />} />
-          <Route
-            path="/collectors"
-            element={user ? <Collectors /> : <Navigate to="/" />}
-          />
-          <Route
-            path="/public-users"
-            element={user ? <Users /> : <Navigate to="/" />}
-          />
-          <Route
-            path="/mapview"
-            element={user ? <Feedback /> : <Navigate to="/" />}
-          />
-          <Route
-            path="/feedback"
-            element={user ? <Feedback /> : <Navigate to="/" />}
-          />
-          <Route
-            path="/settings"
-            element={user ? <Feedback /> : <Navigate to="/" />}
-          />
-          <Route
-            path="/dashboard"
-            element={user ? <Dashboard /> : <Navigate to="/" />}
-          />
+          <Route path="/bins" element={<Bin />} />
+          <Route path="/collectors" element={<Collectors />} />
+          <Route path="/public-users" element={<Users />} />
+          <Route path="/mapview" element={<Feedback />} />
+          <Route path="/feedback" element={<Feedback />} />
+          <Route path="/settings" element={<Feedback />} />
+          <Route path="/dashboard" element={<Dashboard />} />
           <Route
             path="/"
             element={!user ? <Login /> : <Navigate to="/dashboard" />}
@@ -57,3 +39,48 @@ const App = () => {
 };
 
 export default App;
+
+//   return (
+//     <div>
+//       <BrowserRouter>
+//         <Routes>
+//           <Route path="/bins" element={user ? <Bin /> : <Navigate to="/" />} />
+//           <Route
+//             path="/collectors"
+//             element={user ? <Collectors /> : <Navigate to="/" />}
+//           />
+//           <Route
+//             path="/public-users"
+//             element={user ? <Users /> : <Navigate to="/" />}
+//           />
+//           <Route
+//             path="/mapview"
+//             element={user ? <Feedback /> : <Navigate to="/" />}
+//           />
+//           <Route
+//             path="/feedback"
+//             element={user ? <Feedback /> : <Navigate to="/" />}
+//           />
+//           <Route
+//             path="/settings"
+//             element={user ? <Feedback /> : <Navigate to="/" />}
+//           />
+//           <Route
+//             path="/dashboard"
+//             element={user ? <Dashboard /> : <Navigate to="/" />}
+//           />
+//           <Route
+//             path="/"
+//             element={!user ? <Login /> : <Navigate to="/dashboard" />}
+//           />
+//           <Route
+//             path="/register"
+//             element={!user ? <Register /> : <Navigate to="/dashboard" />}
+//           />
+//         </Routes>
+//       </BrowserRouter>
+//     </div>
+//   );
+// };
+
+// export default App;
