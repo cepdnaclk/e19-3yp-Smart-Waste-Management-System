6b4a4084ab3ca52902b8dfaec8c894450232eb44
Sayumi Muthukumarana <115540047+sayumimuthu@users.noreply.github.com>
2024-01-09 18:36:43 +0530

    Dashboard page updated

--- web-app/frontend/src/components/Dashboard/Home.jsx
+++ web-app/frontend/src/components/Dashboard/Home.jsx
@@ -11,15 +11,17 @@ function Home() {
         {" "}
         <br />
         <br />
-        <div className="col-md-4 box" style={style.box}>
-          <FontAwesomeIcon icon={faUsers} /> Users: 100
-        </div>
-        <div className="col-md-4 box" style={style.box}>
-          <FontAwesomeIcon icon={faTrash} /> Bins: 50
-        </div>
-        <div className="col-md-4 box" style={style.box}>
-          <FontAwesomeIcon icon={faStar} /> Rating: 4.5
-        </div>
+          <div className="row justify-content-center">
+            <div className="col-md-4 box" style={style.box}>
+              <FontAwesomeIcon icon={faUsers} /> Users: 100
+            </div>
+            <div className="col-md-4 box" style={style.box}>
+              <FontAwesomeIcon icon={faTrash} /> Bins: 50
+            </div>
+            <div className="col-md-4 box" style={style.box}>
+              <FontAwesomeIcon icon={faStar} /> Rating: 4.5
+            </div>
+          </div>
       </div>
 
       <div className="row">
@@ -102,6 +104,10 @@ const style = {
     fontSize: "24px",
     fontWeight: "bold",
     padding: "10px",
+  },
+  row: {
+    backgroundColor: "#f2f2f2",
+    textAlign: "center",
   },
 };
 
