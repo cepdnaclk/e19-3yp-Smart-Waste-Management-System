7a2050033c3c59ad2e567ade212f038e33c06ef2
Hamshi Mohan <e19244@eng.pdn.ac.lk>
2024-01-28 12:57:55 +0530

    Home screen changed

--- mobile-app/frontend/navigation/AppNavigation.js
+++ mobile-app/frontend/navigation/AppNavigation.js
@@ -8,7 +8,7 @@ import RegisterScreen from '../screens/RegisterScreen'
 import ReportScreen from '../screens/ReportScreen'
 import BinMap from '../screens/BinMap'
 import Aws from '../screens/Aws'
-import ProfileCollector from '../screens/ProfileCollector'
+import Notification from '../screens/Notification'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import { NavigationContainer } from '@react-navigation/native'
 import { AuthContext } from '../context/AuthContext'
@@ -68,8 +68,8 @@ const AppNavigation = () => {
         />
         
         <Stack.Screen
-          name="ProfileCollector"
-          component={ProfileCollector}
+          name="Notification"
+          component={Notification}
           options={{ headerShown: false }}
         />
         <Stack.Screen
